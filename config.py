f909f118aa0bba85b0acde58539e481291367f19
Joakim <jwa018@post.uit.no>
2021-04-23 21:35:22 +0200

    updated

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ WARMUP_PROPORTION = 0.1
 SEED = 42
 
 LEARNING_RATE = 5e-5
-WEIGHT_DECAY = 0.30
+WEIGHT_DECAY = 0.10
 
 CHARACTERISTIC = 7  # see make_binary_df in dataset.py
 CHARACTERISTIC_NAME = 'unhealthy'
