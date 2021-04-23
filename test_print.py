f14cea6bf485d5fbd8ba0f75d6decc28557901c3
Joakim <jwa018@post.uit.no>
2021-04-23 11:28:24 +0200

    updated

--- test_print.py
+++ test_print.py
@@ -1,7 +1,7 @@
 import csv
 print('Success')
 
-with open('usunne-samtaler/data/norwegian.val.csv') as f:
+with open('/data/norwegian.val.csv') as f:
     reader = csv.reader(f)
     for row in reader:
         print(row)
